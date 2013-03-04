2c48a07d17fec8ada4f224c462146c40c753ea5b
Tim Watson <tim@macbook-retina.local>
2013-03-03 23:27:35 -0600

    making roots my own

--- lib/config.php
+++ lib/config.php
@@ -2,7 +2,7 @@
 /**
  * Enable theme features
  */
-add_theme_support('root-relative-urls');    // Enable relative URLs
+//add_theme_support('root-relative-urls');    // Enable relative URLs
 add_theme_support('rewrites');              // Enable URL rewrites
 add_theme_support('h5bp-htaccess');         // Enable HTML5 Boilerplate's .htaccess
 add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's top navbar
